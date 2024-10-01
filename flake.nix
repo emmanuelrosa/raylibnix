f8af5a5242fe456251ca8ce207f50cfb1b822962
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2024-10-01 07:02:46 -0400

    removed patched raylib
    The audio issue has been fixed upstream.

--- flake.nix
+++ flake.nix
@@ -8,27 +8,14 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachSystem [ utils.lib.system.x86_64-linux ] (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-
-      # The zenity package was recently moved out of the gnome package set.
-      # Therefore, look for zenity to be either in pkgs.gnome or pkgs.
-      zenity = if builtins.hasAttr "zenity" pkgs.gnome then pkgs.gnome.zenity else pkgs.zenity;
     in
     {
       packages = {
-        raylib = pkgs.callPackage ./pkgs/raylib { };
-        rguilayout = pkgs.callPackage ./pkgs/rguilayout { inherit zenity; };
-
-        rfxgen = pkgs.callPackage ./pkgs/rfxgen {
-          inherit zenity; 
-          raylib = self.packages.${system}.raylib;
-        };
-
-        rguiicons = pkgs.callPackage ./pkgs/rguiicons { inherit zenity; };
-        riconpacker = pkgs.callPackage ./pkgs/riconpacker { inherit zenity; };
-
-        raylib-games = pkgs.raylib-games.override {
-          raylib = self.packages.${system}.raylib;
-        };
+        rguilayout = pkgs.callPackage ./pkgs/rguilayout { };
+        rfxgen = pkgs.callPackage ./pkgs/rfxgen { };
+        rguiicons = pkgs.callPackage ./pkgs/rguiicons { };
+        riconpacker = pkgs.callPackage ./pkgs/riconpacker { };
+        raylib-games = pkgs.raylib-games;
       };
     }
   );
